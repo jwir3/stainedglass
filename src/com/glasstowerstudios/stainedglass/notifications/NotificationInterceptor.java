882097feb2868af8da3bb1d5ad6e0594083a27eb
Scott Johnson <sjohnson@jingit.com>
2014-02-10 18:43:55 -0600

    Updated project build settings.

---
+++ src/com/glasstowerstudios/stainedglass/notifications/NotificationInterceptor.java
@@ -0,0 +1,16 @@
+package com.glasstowerstudios.stainedglass.notifications;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class NotificationInterceptor extends AccessibilityService{
+  @Override
+  public void onAccessibilityEvent(AccessibilityEvent event) {
+
+  }
+
+  @Override
+  public void onInterrupt() {
+
+  }
+}
