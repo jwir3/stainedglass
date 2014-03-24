27047454ed5e4e10674c32c0a23a7fe058d24672
Scott Johnson <jaywir3@gmail.com>
2014-03-23 19:24:05 -0500

    Issue #9: Added a unit test for the NotificationInterceptor class.
    The unit test does not register with the NotificationManager nor does it
    send notifications to the android notification system. Instead, this unit test
    tests the internal logic of the NotificationInterceptor.

--- stainedglass/src/instrumentTest/java/com/glasstowerstudios/stainedglass/test/notifications/NotificationInterceptorTest.java
+++ stainedglass/src/instrumentTest/java/com/glasstowerstudios/stainedglass/test/notifications/NotificationInterceptorTest.java
@@ -1,15 +1,20 @@
 package com.glasstowerstudios.stainedglass.test.notifications;
 
 import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.service.notification.StatusBarNotification;
 import android.support.v4.app.NotificationCompat;
 import android.test.ServiceTestCase;
 
+import com.glasstowerstudios.stainedglass.R;
 import com.glasstowerstudios.stainedglass.notifications.NotificationBundle;
 import com.glasstowerstudios.stainedglass.notifications.NotificationInterceptor;
 
+import java.lang.reflect.Method;
+import java.util.Date;
 import java.util.List;
 
 public class NotificationInterceptorTest extends ServiceTestCase<NotificationInterceptor> {
@@ -19,34 +24,88 @@ public class NotificationInterceptorTest extends ServiceTestCase<NotificationInt
   }
 
   @Override
-  public void setUp() {
-    setupService();
+  public void setUp() throws Exception {
+    super.setUp();
   }
 
-  public void tearDown() {
+  public void tearDown() throws Exception {
     shutdownService();
+    super.tearDown();
   }
 
-  public void testServiceStarted() {
-    Service s = getService();
+  public void testServiceStartedAndDestroyed() {
+    Intent testIntent = new Intent(getContext(), NotificationInterceptor.class);
+    startService(testIntent);
+
+    NotificationInterceptor s = getService();
 
     assertNotNull("Service should be non-null", s);
+    assertTrue(s.isReady());
+
+    shutdownService();
+    assertFalse(s.isReady());
   }
 
+  public void testServiceBoundAndDestroyed() {
+    Intent testIntent = new Intent(getContext(), NotificationInterceptor.class);
+    bindService(testIntent);
+
+    NotificationInterceptor s = getService();
+
+    assertNotNull("Service should be non-null", s);
+    assertTrue(s.isReady());
+
+    shutdownService();
+    assertFalse(s.isReady());
+  }
+
+  public void testMultipleCalls() {
+    Intent testIntent = new Intent(getContext(), NotificationInterceptor.class);
+
+    startService(testIntent);
+    startService(testIntent);
+
+    assertEquals(1, getService().getNumOnCreates());
+    assertEquals(2, getService().getNumOnStartCommands());
+
+    shutdownService();
+
+    assertFalse(getService().isReady());
+  }
+
+
   public void testNotificationIntercepted() throws Exception {
+    bindService(new Intent(getContext(), NotificationInterceptor.class));
+
     // Our service is already started. Create and send a notification.
     NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext());
     builder.setContentTitle("Hello World");
+    builder.setSmallIcon(R.drawable.ic_launcher);
+    builder.setContentText("What's up?");
     Notification notif = builder.build();
-    NotificationManager mgr =
-        (NotificationManager)getContext().getSystemService(Context.NOTIFICATION_SERVICE);
-    mgr.notify(1492, notif);
+//    NotificationManager mgr =
+//        (NotificationManager)getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+//    mgr.notify(1492, notif);
 
     // Now, get our service and make sure that the notification was intercepted.
-    NotificationInterceptor iceptor = getService();
-    List<NotificationBundle> notifications = iceptor.getNotificationsSeen();
+    // We're sort of mocking this out, because we can't get it to connect _quite_ right at this
+    // time.
+    final NotificationInterceptor interceptor = getService();
+    UserManager userManager = (UserManager)getSystemContext().getSystemService(Context.USER_SERVICE);
+    Method getUserHandle = UserManager.class.getMethod("getUserHandle");
+    int userHandle = (Integer) getUserHandle.invoke(userManager);
+
+    UserHandle uh = userManager.getUserForSerialNumber(userHandle);
+    StatusBarNotification received = new StatusBarNotification("com.glasstowerstudios.stainedglass", "com.glasstowerstudios.stainedglass.test", 1492, "NotificationInterceptorTest", 24, 98, 100, notif, uh, new Date().getTime());
+    interceptor.onNotificationPosted(received);
+
+    List<NotificationBundle> notifications = interceptor.getNotificationsSeen();
 
     assertEquals(1, notifications.size());
+
+//    mgr.cancel(1492);
   }
+
+
 
 }
--- stainedglass/src/main/java/com/glasstowerstudios/stainedglass/notifications/NotificationInterceptor.java
+++ stainedglass/src/main/java/com/glasstowerstudios/stainedglass/notifications/NotificationInterceptor.java
@@ -1,6 +1,9 @@
 package com.glasstowerstudios.stainedglass.notifications;
 
-import android.content.IntentFilter;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
@@ -10,68 +13,134 @@ import java.util.List;
 
 public class NotificationInterceptor extends NotificationListenerService {
 
+  // === [ Public API ] ============================================================================
+
+  public NotificationInterceptor() {
+    mIsReady = false;
+    mOnStartCommands = 0;
+    mOnCreates = 0;
+  }
+
   // === [ Android Event Handlers ] ================================================================
 
   @Override
   public void onCreate() {
     super.onCreate();
-    Log.d(LOGTAG, "Notification interceptor created");
+
+    Log.d(LOGTAG, "NotificationInterceptor.onCreate() called");
     mNotificationsSeen = new ArrayList<NotificationBundle>();
+    mIsReady = true;
+    mOnCreates++;
 
     // We'll need the broadcast receiver to communicate with the main activity/app.
     // The main app will also need one to receive messages from us.
-    mReceiver = new NotificationInterceptorReceiver();
-    IntentFilter filter = new IntentFilter();
-    filter.addAction("com.glasstowerstudios.stainedglass.notifications.NOTIFY_LED_BLINK");
-    registerReceiver(mReceiver, filter);
+//    mReceiver = new NotificationInterceptorReceiver();
+//    IntentFilter filter = new IntentFilter();
+//    filter.addAction("com.glasstowerstudios.stainedglass.notifications.NOTIFY_LED_BLINK");
+//    registerReceiver(mReceiver, filter);
+  }
+
+  @Override
+  public int onStartCommand(Intent aIntent, int aFlags, int aStartId) {
+    mOnStartCommands++;
+    return super.onStartCommand(aIntent, aFlags, aStartId);
   }
 
   @Override
   public void onDestroy() {
+    Log.d(LOGTAG, "Calling onDestroy");
+    mIsReady = false;
     super.onDestroy();
-    unregisterReceiver(mReceiver);
+//    unregisterReceiver(mReceiver);
   }
 
 
   @Override
   public void onNotificationPosted(StatusBarNotification aNotification) {
     Log.d(LOGTAG, "onNotificationPosted called!");
-//    final String packageName = String.valueOf(aNotification.getPackageName());
-//    PackageManager packageManager = this.getPackageManager();
-//
-//    // We want the icon and name for the application sending the notification.
-//    try {
-//      ApplicationInfo appInfo = this.getPackageManager().getApplicationInfo(packageName, 0);
-//      String appName = packageManager.getApplicationLabel(appInfo).toString();
-//      Drawable appIcon = packageManager.getApplicationIcon(appInfo);
-//
-//      mNotificationsSeen.add(new NotificationBundle(appIcon, packageName, appName));
-//    } catch (android.content.pm.PackageManager.NameNotFoundException e) {
-//      // How can we get a name not found exception from an event notification?
-//      Log.e(LOGTAG, "Unable to find package with name: " + packageName);
-//
-//      // We could use a toast here, if we're in one of our activities, but since we're in a
-//      // service, we can't do that.
-//      throw new RuntimeException(e);
-//    }
 
+    // We want the icon and name for the application sending the notification.
+    try {
+      NotificationBundle bundle = getBundleFromStatusBarNotification(aNotification);
+      mNotificationsSeen.add(bundle);
+    } catch (NotificationTranslationException e) {
+      Log.e(LOGTAG, "Error encountered while trying to translate notification", e);
+    }
   }
 
   @Override
   public void onNotificationRemoved(StatusBarNotification aNotification) {
-
+    Log.d(LOGTAG, "onNotificationRemoved() called!");
   }
 
   // === [ Public API ] ============================================================================
+
+  public boolean isReady() {
+    return mIsReady;
+  }
+
+  public int getNumOnCreates() {
+    return mOnCreates;
+  }
+
+  public int getNumOnStartCommands() {
+    return mOnStartCommands;
+  }
+
+
   public List<NotificationBundle> getNotificationsSeen() {
+//    refreshNotifications();
     return mNotificationsSeen;
   }
 
 
+  // === [ Private API ] ===========================================================================
+
+  private class NotificationTranslationException extends Exception {
+
+  }
+
+  private NotificationBundle
+  getBundleFromStatusBarNotification(StatusBarNotification aNotification)
+  throws NotificationTranslationException {
+    final String packageName = String.valueOf(aNotification.getPackageName());
+    PackageManager packageManager = this.getPackageManager();
+
+    try {
+      ApplicationInfo appInfo = this.getPackageManager().getApplicationInfo(packageName, 0);
+      String appName = packageManager.getApplicationLabel(appInfo).toString();
+      Drawable appIcon = packageManager.getApplicationIcon(appInfo);
+
+      return new NotificationBundle(appIcon, packageName, appName);
+    } catch (android.content.pm.PackageManager.NameNotFoundException e) {
+      // How can we get a name not found exception from an event notification?
+      Log.e(LOGTAG, "Unable to find package with name: " + packageName);
+
+      // We could use a toast here, if we're in one of our activities, but since we're in a
+      // service, we can't do that.
+      throw new NotificationTranslationException();
+    }
+  }
+
+  private void refreshNotifications() {
+    StatusBarNotification[] notifications = getActiveNotifications();
+    for (StatusBarNotification notif : notifications) {
+      try {
+        NotificationBundle bundle = getBundleFromStatusBarNotification(notif);
+        mNotificationsSeen.add(bundle);
+      } catch (NotificationTranslationException e) {
+        Log.e(LOGTAG, "Error encountered while trying to translate notification", e);
+      }
+    }
+  }
   // === [ Private Member Variables ] ==============================================================
 
   private List<NotificationBundle> mNotificationsSeen;
-  private NotificationInterceptorReceiver mReceiver;
+//  private NotificationInterceptorReceiver mReceiver;
+
+  private boolean mIsReady;
+  private int mOnCreates;
+  private int mOnStartCommands;
 
   private static final String LOGTAG = "NotificationInterceptor";
 }
